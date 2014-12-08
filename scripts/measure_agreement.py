5a9515e57e1ee9df4ee017b63c7a11195966ed89
Wladimir Sidorenko <wlsidorenko@gmail.com>
2014-12-08 01:58:08 +0100

    script measure agreement updated

--- scripts/measure_agreement.py
+++ scripts/measure_agreement.py
@@ -10,6 +10,7 @@ script_name [OPTIONS] src_dir anno_dir1 anno_dir2
 ##################################################################
 # Libraries
 import argparse
+import glob
 import os
 import sys
 
@@ -37,20 +38,20 @@ SEGMENTS2 = {}
 
 ##################################################################
 # Methods
-def _read_anno_file(a_anno_fname, a_chck_flags, a_rst):
-    """
-    Read annotation file and return a dictionary of annotation elements.
+# def _read_anno_file(a_anno_fname, a_chck_flags, a_rst):
+#     """
+#     Read annotation file and return a dictionary of annotation elements.
 
-    @param a_anno_fname - name of the 1-st file containing annotation
-    @param a_chck_flags - flags specifying which elements should be checked
-    @param a_rst - RST tree to be populated from the given file
+#     @param a_anno_fname - name of the 1-st file containing annotation
+#     @param a_chck_flags - flags specifying which elements should be checked
+#     @param a_rst - RST tree to be populated from the given file
 
-    @return \c void
-    """
-    with open(a_anno_fname) as ifile:
-        fields = {}
-        for line in ifile:
-            a_rst.parse_tsv(line.decode(ENCODING))
+#     @return \c void
+#     """
+#     with open(a_anno_fname) as ifile:
+#         fields = {}
+#         for line in ifile:
+#             a_rst.parse_tsv(line.decode(ENCODING))
 
 def output_agreement(a_kappa_stat = KAPPA_STAT, a_header = "total"):
     """
@@ -70,7 +71,80 @@ def measure_agreement(a_src_fname, a_anno1_fname, a_anno2_fname, a_chck_flags):
 
     @return \c void
     """
-    pass
+    # read input file
+    messages = {}
+    fields = []
+    with open(a_src_fname) as ifile:
+        for line in ifile:
+            line = line.decode(ENCODING).strip()
+            if not line:
+                continue
+            fields = line.split(FIELD_SEP)
+            messages[fields[0]] = fields[1]
+    # read first annotation file
+    seg1 = {}
+    msg_id = -1
+    with open(a_anno1_fname) as ifile:
+        for line in ifile:
+            line = line.decode(ENCODING).strip()
+            if not line:
+                continue
+            fields = line.split(FIELD_SEP)
+            if fields[0] == INTNID and len(fields) > 16 and \
+                    fields[4].split(VALUE_SEP)[1] == "text":
+                msg_id = fields[2]
+                if msg_id not in seg1:
+                    seg1[msg_id] = []
+                assert fields[15].split(VALUE_SEP)[0] == "offsets", "Wrong index of offset field"
+                seg1[msg_id].append([int(v) for v in fields[15].split(VALUE_SEP)[1:]])
+    # sort segmentations
+    for msg_id in seg1:
+        seg1[msg_id].sort(key = lambda el: el[0])
+    # read second annotation file
+    seg2 = {}
+    with open(a_anno2_fname) as ifile:
+        for line in ifile:
+            line = line.decode(ENCODING).strip()
+            if not line:
+                continue
+            fields = line.split(FIELD_SEP)
+            if fields[0] == INTNID and len(fields) > 16 and \
+                    fields[4].split(VALUE_SEP)[1] == "text":
+                msg_id = fields[2]
+                if msg_id not in seg2:
+                    seg2[msg_id] = []
+                assert fields[15].split(VALUE_SEP)[0] == "offsets", "Wrong index of offset field"
+                seg2[msg_id].append([int(v) for v in fields[15].split(VALUE_SEP)[1:]])
+    # sort segmentations
+    for msg_id in seg2:
+        seg2[msg_id].sort(key = lambda el: el[0])
+    # compare segmentations
+    for msg_id, msg in messages.iteritems():
+        if (msg_id in seg1 and msg_id in seg2) or \
+            (msg_id not in seg1 and msg_id not in seg2):
+            print >> sys.stderr, "WARNING: Message {:s} segmented by only one annotator".format(msg_id)
+
+        if msg_id not in seg1 or msg_id not in seg2:
+            continue
+
+        # check if both segmentations are equal
+        if len(seg1[msg_id]) == len(seg2[msg_id]):
+            for ((start1, end1),(start2, end2)) in zip(seg1[msg_id], seg2[msg_id]):
+                if msg[start1:end1].strip() != msg[start2:end2].strip():
+                    break
+            else:
+                continue
+
+        print u"{:s}\t{:s}".format(msg_id, msg).encode(ENCODING)
+        seg_start = seg_end = 0
+        # print first segmentation
+        for seg_start, seg_end in seg1[msg_id]:
+            print u"{:s}<>".format(msg[seg_start:seg_end]).encode(ENCODING),
+        print u"\n"
+        # print second segmentation
+        for seg_start, seg_end in seg2[msg_id]:
+            print u"{:s}<>".format(msg[seg_start:seg_end]).encode(ENCODING),
+        print u"\n"
 
 def main(argv):
     """
@@ -82,22 +156,21 @@ def main(argv):
     """
     global ENCODING
     # define command line arguments
-    aparser = argparse.ArgumentParser(description = "Script for measuring corpus agreement on RST.")
+    argparser = argparse.ArgumentParser(description = "Script for measuring corpus agreement on RST.")
     # optional arguments
-    aparser.add_argument("-e", "--encoding", help = """encoding of input document""",
+    argparser.add_argument("-e", "--encoding", help = """encoding of input document""",
                          type = str, default = "utf-8")
-    aparser.add_argument("--type", help = """type of element (relation) for which to measure
+    argparser.add_argument("--type", help = """type of element (relation) for which to measure
 the agreement""", choices = [SEGMENTS, NUCLEARITY, RELATIONS], type = str, action = "append")
-    aparser.add_argument("--src-ptrn", help = "shell pattern of source files", type = str,
-                         defaul = "*")
-    aparser.add_argument("--anno-sfx", help = "extension of annotation files", type = str,
-                         defaul = "")
-    aparser.add_argument("--segment-chance", help = "extension of annotation files", type = int,
-                         defaul = "")
+    argparser.add_argument("--src-ptrn", help = "shell pattern of source files", type = str,
+                         default = "*")
+    argparser.add_argument("--anno-sfx", help = "extension of annotation files", type = str,
+                         default = "")
+    argparser.add_argument("--segment-chance", help = "extension of annotation files", action = "store_true")
     # mandatory arguments
-    aparser.add_argument("src_dir", help = "directory with source files used for annotation")
-    aparser.add_argument("anno1_dir", help = "directory with annotation files for first annotator")
-    aparser.add_argument("anno2_dir", help = "directory with annotation files for second annotator")
+    argparser.add_argument("src_dir", help = "directory with source files used for annotation")
+    argparser.add_argument("anno1_dir", help = "directory with annotation files for first annotator")
+    argparser.add_argument("anno2_dir", help = "directory with annotation files for second annotator")
     args = argparser.parse_args(argv)
     # set parameters
     ENCODING = args.encoding
@@ -122,11 +195,11 @@ the agreement""", choices = [SEGMENTS, NUCLEARITY, RELATIONS], type = str, actio
         if not os.path.isfile(src_fname):
             continue
         # check annotation files corresponding to the given source file
-        src_fname_base = os.path.splitext(os.path.basename())[0]
-        anno1_fname = os.path.join(args.anno1_dir, src_fname_base + args.anno_sfx)
+        src_fname_base = os.path.splitext(os.path.basename(src_fname))[0]
+        anno1_fname = glob.glob(os.path.join(args.anno1_dir, src_fname_base + args.anno_sfx) + "*")[0]
         if not os.path.isfile(anno1_fname) or not os.access(anno1_fname, os.R_OK):
             continue
-        anno2_fname = os.path.join(args.anno2_dir, src_fname_base + args.anno_sfx)
+        anno2_fname = glob.glob(os.path.join(args.anno2_dir, src_fname_base + args.anno_sfx) + "*")[0]
         if not os.path.isfile(anno2_fname) or not os.access(anno2_fname, os.R_OK):
             continue
         # measure agreement for the given annotation files
@@ -137,4 +210,4 @@ the agreement""", choices = [SEGMENTS, NUCLEARITY, RELATIONS], type = str, actio
 ##################################################################
 # Main
 if __name__ == "__main__":
-    return main(sys.argv[1:])
+    main(sys.argv[1:])
